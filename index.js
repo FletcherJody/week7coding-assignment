ad834f5c269abfe35dc7d949458d77b1064a7e95
FletcherJody <153571753+FletcherJody@users.noreply.github.com>
2024-02-03 22:33:03 -0700

    coding complete

--- index.js
+++ index.js
@@ -89,33 +89,100 @@ for (let i = 0; i < nameLengths.length; i++) {
     sumLetters += nameLengths[i];
 };
 console.log("     6. the sum of the nameLengths array is: " + sumLetters);
+console.log("------------------------------");
 
 // 7.
 // Write a function that takes two parameters, word and n, as arguments and returns the word concatenated to itself n number of times. 
 // --(i.e. if I pass in 'Hello' and 3, I would expect the function to return 'HelloHelloHello').
+console.log("7. Write a function that takes 2 parameters and returns the word concatenated to itself n times.")
 // -------------------------------------------------------------------------------
+function repeat(word, n) {
+    let result = ' ';
+    for (let i = 0; i < n; i++) {
+        result += word;
+    }
+    console.log("     7. word concatenated 7x: " + (result))
+} repeat("Wowza", 7);
+console.log("------------------------------");
 
 // 8.
 // Write a function that takes two parameters, firstName and lastName, and returns a full name. 
 // ----The full name should be the first and the last name separated by a space.
+console.log("8. Write a function that takes 2 parameters and returns a full name, first and last separated by a space.")
 // ---------------------------------------------------------------------------------
+function hello(firstName, lastName) {
+    console.log("     8. this is the full name: " + `${firstName} ${lastName}`)
+}
+hello('Bob', 'Jones');
+console.log("------------------------------");
 
 // 9.
 // Write a function that takes an array of numbers and returns true if the sum of all the numbers in the array is greater than 100.
+console.log("9. Write a function that takes an array of numbers and returns true if the sum of all the numbers in the array is greater than 100.")
 // -------------------------------------------------------------------------------------
+function totalArray(arr) {
+    let total = 0;
+    for (let i = 0; i < arr.length; i++) {
+        total += arr[i];
+    }
+    return total > 100;
+};
+let thisArray = [28, 27, 29, 25]; //confirmed false appeared when total is <= 100
+console.log("     9. return true if total is >100: " + totalArray(thisArray))
+console.log("------------------------------");
+
 
 // 10.
 // Write a function that takes an array of numbers and returns the average of all the elements in the array.
+console.log("10. Write a function that takes an array of numbers and returns the average of all the elements in the array.")
 // ------------------------------------------------------------------------------------
+function arrayAvg(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum / arr.length;
+};
+let thatArray = [14, 16, 100, 34, 36]
+console.log("     10. the average of the array is: " + arrayAvg(thatArray));
+console.log("------------------------------");
 
 // 11.
 // Write a function that takes two arrays of numbers and returns true if the average of the elements in the first array is greater than the average of the elements in the second array.
+console.log("11. Write a function that compares the averages of two arrays and returns true if array 1 is greater than array 2.")
 // ---------------------------------------------------------------------------------------
+function compareArrAvg(array1, array2) {
+    const average1 = array1.reduce((acc, curr) => acc + curr, 0) / array1.length;
+    const average2 = array2.reduce((acc, curr) => acc + curr, 0) / array2.length;
+
+    return average1 > average2
+}
+const arr1 = [7, 8, 9];
+const arr2 = [6, 9, 3];
+console.log("     11. the first array's average is greater than the 2nd array's average: " + compareArrAvg(arr1, arr2));
+console.log("------------------------------");
 
 // 12.
 // Write a function called willBuyDrink that takes a boolean isHotOutside, and a number moneyInPocket, and returns true if it is hot outside and if moneyInPocket is greater than 10.50.
-// 
+console.log("12. Write a function called willBuyDrink that takes a boolean isHotOutside, and a number moneyInPocket, and returns true if it is hot outside and if moneyInPocket is greater than 10.50.")
+// ---------------------------------------------------------------------------------------
+const willBuyDrink = (isHotOutside, moneyInPocket) => isHotOutside && moneyInPocket > 10.50;
+console.log("     12. Will buy drink: " + willBuyDrink(true, 15));
+console.log("------------------------------");
 
 // 13.
 // Create a function of your own that solves a problem. 
 // ----In comments, write what the function does and why you created it.
+console.log("13. Create a function that solves a problem.")
+// ---------------------------------------------------------------------------------------
+function flipCoin() {
+    let randomNumber = Math.random();
+
+    if (randomNumber > 0.5) {
+        return "Heads";
+    } else {
+        return "Tails"
+    }
+}
+console.log("If it's heads, we go out to eat; if it's tails, we cook at home: " + flipCoin());
+// this function takes the place of an actual coin flip and will help me decide if we are going out to eat or staying home to cook.
